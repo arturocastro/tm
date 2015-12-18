f202eb71e38f993302b8436014f18d5f29c46f48
Arturo Isai Castro Perpuli <arturo.perpuli@arm.com>
2015-12-18 16:10:48 +0000

    Add opts and args

--- test.rb
+++ test.rb
@@ -1,34 +1,63 @@
 require 'rubygems'
 require 'optparse'
+require 'ostruct'
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/poltergeist'
 
-options = {}
-OptionParser.new do |opts|
-  opts.banner = "Usage: test.rb [-chv] -u<user> -p<pass>"
+def self.parse(args)
+  options = OpenStruct.new
+  options.browser = "firefox"
+  options.user = ""
+  options.pass = ""
+  options.verbose = false
 
-  opts.on("-v", "Verbose") do |v|
-    options[:verbose] = v
+  opt_parser = OptionParser.new do |opts|
+    opts.banner = "Usage: example.rb [options]"
+
+    opts.separator ""
+    opts.separator "Specific options:"
+
+    opts.on("-uUSER", "--user USER", "User") do |u|
+      options.user = u
+    end
+
+    opts.on("-pPASS", "--pass PASS", "Password") do |p|
+      options.pass = p
+    end
+
+    opts.on("-bBROWSER", "--browser BROWSER", "User [firefox|chrome|phantomjs]") do |b|
+      options.browser = b
+    end
+
+    opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+      options.verbose = v
+    end
+
+    opts.separator ""
+    opts.separator "Common options:"
+
+    opts.on_tail("-h", "--help", "Show this message") do
+      puts opts
+      exit
+    end
   end
 
-  opts.on("-c", "Use Chrome") do |c|
-    options[:chrome] = c
-  end
+  opt_parser.parse!(args)
+  options
+end
 
-  opts.on("-h", "Use PhantomJS (headless)") do |h|
-    options[:headless] = h
-  end
+opts = parse(ARGV)
 
 Capybara.run_server = false
 
-if options[:chrome]
+if opts.browser == "chrome"
   Capybara.register_driver :selenium do |app|
     Capybara::Selenium::Driver.new(app, :browser => :chrome)
   end
 end
 
-if options[:headless] 
+if opts.browser == "phantomjs"
   Capybara.default_driver = :poltergeist
   Capybara.javascript_driver = :poltergeist
 else 
@@ -37,22 +66,20 @@ end
 
 Capybara.app_host = 'http://bbsistema.tecmilenio.edu.mx/'
 
-module MyCapybaraTest
-  class Test
-    include Capybara::DSL
-    def test_google
-	visit('/')
-	fill_in('user_id', :with => 'xxx')
-	fill_in('password', :with => 'xxx')
-	click_on('Login')
+class Test
+  include Capybara::DSL
+  def test_google(user, pass)
+    if user.empty? or pass.empty? then abort end
 
-	page.has_content?('CAP.CE00028.5.17NOV2015') do
-	  puts("holy!")
-	end
+    visit('/')
+    fill_in('user_id', :with => user)
+    fill_in('password', :with => pass)
+    click_on('Login')
+    
+    if page.has_content?('CAP.CE00028.5.17NOV2015')
+      puts("holy!")
     end
   end
 end
 
-t = MyCapybaraTest::Test.new
-t.test_google
-
+Test.new.test_google(opts.user, opts.pass)
