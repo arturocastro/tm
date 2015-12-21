c6c8a528d853298c769442169da914ddc8ed0b43
Arturo Isaí Castro Pérpuli <arturo.castrop@live.com>
2015-12-21 12:40:09 +0000

    Remove code from opt in test

--- test.rb
+++ test.rb
@@ -1,51 +1,9 @@
 require 'rubygems'
-require 'optparse'
-require 'ostruct'
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/poltergeist'
+require_relative('opt')
 
-def self.parse(args)
-  options = OpenStruct.new
-  options.browser = "firefox"
-  options.user = ""
-  options.pass = ""
-  options.verbose = false
-
-  opt_parser = OptionParser.new do |opts|
-    opts.banner = "Usage: example.rb [options]"
-
-    opts.separator ""
-    opts.separator "Specific options:"
-
-    opts.on("-uUSER", "--user USER", "User") do |u|
-      options.user = u
-    end
-
-    opts.on("-pPASS", "--pass PASS", "Password") do |p|
-      options.pass = p
-    end
-
-    opts.on("-bBROWSER", "--browser BROWSER", "User [firefox|chrome|phantomjs]") do |b|
-      options.browser = b
-    end
-
-    opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-      options.verbose = v
-    end
-
-    opts.separator ""
-    opts.separator "Common options:"
-
-    opts.on_tail("-h", "--help", "Show this message") do
-      puts opts
-      exit
-    end
-  end
-
-  opt_parser.parse!(args)
-  options
-end
 
 $opts = parse(ARGV)
 
