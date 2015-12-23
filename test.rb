3f58b49e5486612675fd87b6b84652593b1b5a2d
Arturo Isai Castro Perpuli <arturo.perpuli@arm.com>
2015-12-23 16:12:02 +0000

    Revert to specifying phantomjs path

--- test.rb
+++ test.rb
@@ -18,12 +18,12 @@ end
 if $options.browser == "phantomjs"
   path = $options.phantomjs_path
 
-  #if !path
-    o = {:phantomjs => "/usr/local/tm/phantomjs", :port => 15002}
+  if !path
+    o = {:phantomjs => path, :debug => true}
     Capybara.register_driver :poltergeist do |app|
       Capybara::Poltergeist::Driver.new(app, o)
     end
-  #end
+  end
 
   Capybara.default_driver = :poltergeist
   Capybara.javascript_driver = :poltergeist
